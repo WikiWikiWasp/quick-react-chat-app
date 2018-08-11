dea93be1dd0e968c06aa19e0cd5cfa163a72640e
Jason Flinn <jcf70@txstate.edu>
2018-08-11 03:07:05 -0500

    some comments, formatting, MessageList class, Title function, dom render

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
+//test data
+//replace with real data from chatkit later
 const TEST_DATA = [
     {
-        senderID: "max",
+        senderID: "Max",
         text: "suh dude"
     },
     {
-        senderID: "sam",
+        senderID: "Sam",
         text: "suhhhh"
     }
 ]
 
 class App extends React.Component{
     constructor() {
+        //must call super() in constructor in order
+        //to create a stateful component
         super()
         this.state = {
             messages: TEST_DATA
         }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="app">
                 <Title />
                 <MessageList messages={this.state.messages} />
-                <SendMessageForm />
+                //sendermessageform
             </div>
+        );
+    }
+}
+
+class MessageList extends React.Component {
+    render() {
+        return (
+            <ul className="message-list">
+                {this.props.messages.map(message => {
+                    return (
+                        <li key={message.id} className="message">
+                            <div>
+                                {message.senderID}
+                            </div>
+                            <div>
+                                {message.text}
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         )
     }
 }
+
+function Title() {
+  return <p className="title">Test React Chat App</p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
